154540f4b081ab0015e76565c2baa02af64d4570
sandanitin <sandanithin2@gmail.com>
2026-02-16 23:39:21 +0530

    backend

---
+++ models/index.js
@@ -0,0 +1,13 @@
+const Lodge = require('./Lodge');
+const Room = require('./Room');
+const Booking = require('./Booking');
+const User = require('./User');
+const Review = require('./Review');
+
+module.exports = {
+    Lodge,
+    Room,
+    Booking,
+    User,
+    Review
+};
